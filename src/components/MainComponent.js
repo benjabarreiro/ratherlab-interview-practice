086dc2973f32674e4a4bcc477a4053f1c86dd50c
benjabarreiro <barreirobenjamin@gmail.com>
2021-03-19 22:26:11 -0300

    Practice finished

---
+++ src/components/MainComponent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+
+import Home from './Home';
+import About from './About';
+
+import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
+
+function Main() {
+    return(
+        <React.Fragment>
+
+            <Header />
+
+            <Switch>
+            <Route path="/home" component={Home} />
+
+            <Route path="/about" component={About} />
+
+            <Redirect to="/home" />
+            </Switch>
+
+            <Footer />
+
+        </React.Fragment>
+    )
+}
+
+export default withRouter(Main);
